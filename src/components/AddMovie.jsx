b9739f3e93e28cec0ea195eb624617ebc1a554d8
waelkhrdir <37935096+waelkhrdir@users.noreply.github.com>
2021-11-09 17:53:18 +0100

    Add files via upload

---
+++ src/components/AddMovie.jsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+import StarRating from "./StarRating";
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+Modal.setAppElement("#root");
+
+const AddMovie = ({ handleAdd }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  
+  const [form, setForm] = useState({
+    name: "",
+    date: "",
+    image: "",
+    rating: 1,
+  });
+  const handleChange = (e) =>
+    setForm({ ...form, [e.target.name]: e.target.value });
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  const handleRate = (rate) => setForm({ ...form, rating: rate });
+  return (
+    <div>
+      <button className="btn add-movie-btn" onClick={openModal}>
+        Add movie
+      </button>
+      <Modal
+        style={customStyles}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Example Modal"
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            let newMovie = {
+              ...form,
+              id: Math.random(),
+            };
+            handleAdd(newMovie);
+            closeModal();
+          }}
+        >
+          <label>Name</label>
+          <input
+            type="text"
+            value={form.name}
+            name="name"
+            onChange={handleChange}
+            
+          />
+          <label> Date of release </label>
+          <input
+            type="date"
+            value={form.date}
+            name="date"
+            onChange={handleChange}
+          
+          />
+          <label> Rating </label>
+          <StarRating rate={form.rating} handleRating={handleRate} />
+
+          <label>Image</label>
+          <input
+            type="url"
+            value={form.image}
+            name="image"
+            onChange={handleChange}
+          />
+          <div>
+            <button className="btn btn-primary" type="submit">
+              Add
+            </button>
+            <button className="btn btn-danger" onClick={closeModal}>
+              Cancel
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddMovie;
