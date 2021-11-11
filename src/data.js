fbdfae5c79661f628f023cfdb887b1982c544198
waelkhrdir <37935096+waelkhrdir@users.noreply.github.com>
2021-11-11 12:45:59 +0100

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const moviesData = [
+    {
+      id: Math.random(),
+      image: "https://knowledgenuts.com/wp-content/uploads/2014/02/godfather.png",
+      rating: 5,
+      name: "godfather",
+      date: "December 2019",
+    },
+    {
+      id: Math.random(),
+      image: "https://images.squarespace-cdn.com/content/v1/5c1f65fd697a98b026860fd6/1556651543807-FW35U7Q3RRHQ7PC8Y3P9/Avengement-Movie-Poster.jpg",
+      rating: 4,
+      name: "Avengement",
+      date: "April 2018",
+    },
+    {
+      id: Math.random(),
+      image: "https://m.media-amazon.com/images/M/MV5BNTFiNzBlYmEtMTcxZS00ZTEyLWJmYmQtMjYzYjAxNGQwODAzXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+      rating: 4,
+      name: "Venom",
+      date: "November 2019",
+    },
+    {
+      id: Math.random(),
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/b/bd/Bird_Box_%28film%29.png",
+      rating: 3,
+      name: "Bird Box",
+      date: "November 2016",
+    },
+    {
+      id: Math.random(),
+      image:
+        "https://martinave.org/wp-content/uploads/2021/06/echoart-hun-the-irishman-8-scaled.jpg",
+      rating: 2,
+      name: "the irishman",
+      date: "November 2003",
+    },
+  ];
